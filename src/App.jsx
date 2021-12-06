3800a28b8f29f418dd01b7718686b2489bc1e24f
mrshuhrat <shuhratadm1n@gmail.com>
2021-12-06 08:19:10 +0500

    Init eslint with airbnb style guide

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+/* eslint linebreak-style: ["error", "windows"] */
+import './App.css';
+import React from 'react';
+
+const App = function nimadir() {
+  return <div className="App">App.</div>;
+};
+
+export default App;
